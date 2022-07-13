05b2248a8ef3c2d965f9e668a2d504ed75a31cf9
Ali <mulwor.001@yandex.ru>
2022-07-13 18:26:44 +0300

    feat: add components

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,11 +1,15 @@
+import Card from './components/Card'
+
 function App() {
   return (
     <div className="wrapper clear">
-      <div className="overlay">
+      <div style = {{ display: 'none' }} className="overlay">
         <div className="drawer">
-            <h2 className="mb-30"> Корзина </h2>
+            <h2 className="d-flex justify-between mb-30"> Корзина 
+               <img className = "cu-p" src='/img/Krestik.svg' alt="Remove"/> 
+             </h2>
 
-            <div className="cartItem d-flex align-center">
+            <div className="cartItem d-flex align-center mb-20">
               <div 
                 style={{backgroundImage:'url(/img/Sneakers/First-shoes.jpg)' }} className='cartItemImg'>
               </div>
@@ -18,6 +22,36 @@ function App() {
               <img className = "removeBtn" src='/img/Krestik.svg' alt="Remove"/>
             </div>
 
+            <div className="cartItem d-flex align-center">
+              <div 
+                style={{backgroundImage:'url(/img/Sneakers/First-shoes.jpg)' }} 
+                className='cartItemImg'>
+              </div>
+
+              <div className="mr-20 flex">
+                <p className ="mb-5">Мужские Кроссовки Nike Air Max 270</p>
+                <b>12 999 руб.</b>
+              </div>
+              
+              <img className = "removeBtn" src='/img/Krestik.svg' alt="Remove"/>
+            </div>
+
+            <div className="cartTotalBlock">
+                <ul>
+                  <li>
+                      <span>Итого:</span>
+                      <div></div>
+                      <b>21 498 руб.</b>
+                  </li>
+                
+                  <li>
+                      <span>Налог 5%:</span>
+                      <div></div>
+                      <b>1074 руб.</b>
+                  </li>
+                </ul>
+                <button className="greenButton">Оформить заказ <img src="img/Strelka.svg" alt="Arrow"/></button>
+            </div>
         </div>
       </div>
 
@@ -53,12 +87,11 @@ function App() {
         </div>
         
         <div className="d-flex">
+          <Card />
           <div className = "card">
             <div className = 'favorite'>
               <img src="/img/Unlike.svg" alt="Unnactive-like"/>
             </div>
-
-            
             
             <img width ={133} height={112} src = "/img/Sneakers/First-shoes.jpg" alt = "" />
             <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
@@ -120,75 +153,13 @@ function App() {
               </button>
             </div>
           </div>
-
-
-          <div className = "card">
-            <img width ={133} height={112} src = "/img/Sneakers/Fifth-shoes.jpg" alt = "" />
-            <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
-            <div className = "d-flex justify-between align-center">
-              <div className = 'd-flex flex-column'>
-                <span>Цена:</span>
-                <b>12 999</b>
-              </div>
-              
-              <button className='button'>
-                  <img width={11} height={11} src="/img/Vector-plus.svg"/>
-              </button>
-            </div>
-          </div>
-
-
-          <div className = "card">
-            <img width ={133} height={112} src = "/img/Sneakers/Sixth-shoes.jpg" alt = "" />
-            <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
-            <div className = "d-flex justify-between align-center">
-              <div className = 'd-flex flex-column'>
-                <span>Цена:</span>
-                <b>12 999</b>
-              </div>
-              
-              <button className='button'>
-                  <img width={11} height={11} src="/img/Vector-plus.svg"/>
-              </button>
-            </div>
-          </div>
-
-          <div className = "card">
-            <img width ={133} height={112} src = "/img/Sneakers/Sixth-shoes.jpg" alt = "" />
-            <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
-            <div className = "d-flex justify-between align-center">
-              <div className = 'd-flex flex-column'>
-                <span>Цена:</span>
-                <b>12 999</b>
-              </div>
-              
-              <button className='button'>
-                  <img width={11} height={11} src="/img/Vector-plus.svg"/>
-              </button>
-            </div>
-          </div>
-
-
-          <div className = "card">
-            <img width ={133} height={112} src = "/img/Sneakers/Sixth-shoes.jpg" alt = "" />
-            <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
-            <div className = "d-flex justify-between align-center">
-              <div className = 'd-flex flex-column'>
-                <span>Цена:</span>
-                <b>12 999</b>
-              </div>
-              
-              <button className='button'>
-                  <img width={11} height={11} src="/img/Vector-plus.svg"/>
-              </button>
-            </div>
-          </div>
+        
 
         </div>
 
     </div>
   </div>
-  )
+  );
 }
 
 /* Function App() {...} - доступен везде где хочет пользователь. Находится данный код в
@@ -199,4 +170,3 @@ index.js:
 default - это определенная функция, переменная и т.д. которую мы хотим использовать в будущем
 */
 export default App;
-   
---
+++ react-sneakers/src/components/Card.js
@@ -0,0 +1,26 @@
+
+
+function Card() {
+    return (
+      <div className = "card">
+        <div className = 'favorite'>
+          <img src="/img/Unlike.svg" alt="Unnactive-like"/>
+        </div>
+        <img width ={133} height={112} src = "/img/Sneakers/First-shoes.jpg" alt = "" />
+        <h5> Мужские Кроссовки Under Armour Curry 8 </h5>
+    
+        <div className = "d-flex justify-between align-center">
+            <div className = 'd-flex flex-column'>
+                <span>Цена:</span>
+                <b>12 999</b>
+            </div>
+  
+            <button className='button'>
+                <img width={11} height={11} src="/img/Vector-plus.svg"/>
+            </button>
+        </div>
+      </div>
+    );
+}
+
+export default Card;
